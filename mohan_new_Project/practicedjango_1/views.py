353d1d502f45c2091a9f705c2ca00bf18f94019f
Mohann23 <Mohan444a3@gmail.com>
2022-05-07 19:11:11 +0530

    Day_1

--- mohan_new_Project/practicedjango_1/views.py
+++ mohan_new_Project/practicedjango_1/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def welcome(request):     #we can use any varibale name in place of request
-    s="<h1>Hello Welcome to django classes at Nursery level </h1>"
+    s="<h1>Hello Welcome to django classes at Nursery level</h1>"
     return HttpResponse(s)
 
 
