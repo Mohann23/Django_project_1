237087f5683e58ee8b56ac949e3ed03e5bb7bfca
Mohann23 <Mohan444a3@gmail.com>
2022-05-09 17:28:56 +0530

    Day_2

---
+++ datetime_project/test_app_date/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
+# Create your views here.
+def datetimeinfo(request):
+    date = datetime.now()
+    h = int(date.strftime("%H"))   #to get the time in hours
+    msg = "<h1> Hello Guest"
+    if h<12:
+        msg = msg + "Good Morning"
+
+    elif h<16:
+        msg = msg + "Good Afternoon!!!"
+
+    elif h<21:
+        msg = msg + "Good Evening!!!"
+
+    else:
+        msg = msg + "Good Night!!!"
+
+    msg = msg + "</h1><hr>"
+    msg = msg + "<h1> Now server time is: " + str(date) + "</h1>"
+    return HttpResponse(msg)
+
