18c7b7756ff4681b0c4674db7a9e88491db33246
Mohann23 <Mohan444a3@gmail.com>
2022-05-09 16:55:44 +0530

    Day_2

--- mohan_new_Project/mohan_new_Project/urls.py
+++ mohan_new_Project/mohan_new_Project/urls.py
@@ -14,14 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from practicedjango_1 import views
 from Jobs_Assignment import views
 
 
-urlpatterns = [
+
+urlpatterns = {
     path('admin/', admin.site.urls),
+    path('hello/', include(views.welcome)),
     path('hyd', views.hyd_jobs),
     path('bang', views.bang_jobs),
-    path('pune',views.pune),
-]
+    path('pune', views.pune),
+}
